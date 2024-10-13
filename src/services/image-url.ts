1a6d4472b517af6fdb78a63a74d4f8347abeaf14
Merve Balci <mervebalci090@gmail.com>
2024-10-13 12:15:22 -0300

    Getting optimized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,14 @@
+// Optimizing images for the users who have slow internet connection
+
+export default function getCroppedImageUrl( url: string ) {
+  const target = "media/";
+
+  // Finding the index of 'media/'
+  const index = url.indexOf(target) + target.length; // output is integer
+
+  /* To insert the crop parameter:
+    slice the url from the beginning to the index,
+    insert crop parameter,
+    add rest of the url, starting from index till the end */
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+}
