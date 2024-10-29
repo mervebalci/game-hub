ab18394fbf8e123b45b119d83fce9ef48a3e7921
Merve Balci <mervebalci090@gmail.com>
2024-10-29 19:55:28 -0300

    Building search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -9,7 +9,7 @@ export default function ColorModeSwitch() {
         onChange={toggleColorMode}
         colorScheme="green"
       />
-      <Text>Dark Mode</Text>
+      <Text whiteSpace={"nowrap"}>Dark Mode</Text>
     </HStack>
   );
 }
