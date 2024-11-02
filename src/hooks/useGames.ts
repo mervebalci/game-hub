319dea185b0e26270a39bb29860abbdf755ed35d
Merve Balci <mervebalci090@gmail.com>
2024-11-02 19:06:56 -0300

    Adding emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -13,6 +13,7 @@ export interface Game {
   background_image: string;
   parent_platforms: { platform: Platform }[]; // Array of platform object
   metacritic: number;
+  rating_top: number;
 }
 
 const useGames = (gameQuery: GameQuery) => 
